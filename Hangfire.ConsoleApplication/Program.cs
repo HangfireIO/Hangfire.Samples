77d922113ff0b89d411b38c672149215b7f73412
Sergey Odinokov <odinserj@gmail.com>
2014-11-24 17:42:45 +0300

    Add Hangfire.ConsoleApplication project

---
+++ Hangfire.ConsoleApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Common.Logging;
+using Common.Logging.Simple;
+using Microsoft.Owin.Hosting;
+
+namespace Hangfire.ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Configure AppDomain parameter to simplify the config – http://stackoverflow.com/a/3501950/1317575
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data"));
+
+            LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter(LogLevel.Info, true, false, false, "");
+
+            const string endpoint = "http://localhost:12345";
+
+            using (WebApp.Start<Startup>(endpoint))
+            {
+                ConsoleServices.WriteLine();
+                ConsoleServices.WriteLine("Hangfire Server started.");
+                ConsoleServices.WriteLine("Dashboard is available at {0}/hangfire", endpoint);
+                ConsoleServices.WriteLine();
+                ConsoleServices.WriteLine("Type JOB to add a background job.");
+                ConsoleServices.WriteLine("Press ENTER to exit...");
+
+                string command;
+                while ((command = Console.ReadLine()) != String.Empty)
+                {
+                    if ("job".Equals(command, StringComparison.OrdinalIgnoreCase))
+                    {
+                        BackgroundJob.Enqueue(() => ConsoleServices.WriteLine("Background job completed!"));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Hangfire.ConsoleApplication/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.ConsoleApplication.Startup))]
+
+namespace Hangfire.ConsoleApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseWelcomePage("/");
+
+            app.UseHangfire(config =>
+            {
+                var options = new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) };
+                config.UseSqlServerStorage("DefaultConnection", options);
+                config.UseServer();
+            });
+
+            RecurringJob.AddOrUpdate(() => ConsoleServices.WriteLine("Recurring job completed!"), Cron.Minutely);
+        }
+    }
+}
