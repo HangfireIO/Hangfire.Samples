5de6ef65fe9295912f8e7f691c4f55d5670002f2
Sergey Odinokov <odinserj@gmail.com>
2017-08-31 15:21:59 +0300

    Add WebFormsApplication sample

---
+++ Hangfire.WebFormsApplication/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Hangfire.WebFormsApplication.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Hangfire.WebFormsApplication/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hangfire.WebFormsApplication.Startup))]
+namespace Hangfire.WebFormsApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate(
+                () => Console.WriteLine("{0} Recurring job completed successfully!", DateTime.Now.ToString()),
+                Cron.Minutely);
+        }
+    }
+}
